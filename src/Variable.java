17fd54771f3f6ab9b844550a2645bd6e34554224
RyanDJang <jangd6995@gmail.com>
2023-10-06 13:00:15 +0900

    variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Variable {
+public class Variable {
+    public static void main(String[] args) {
+        int num = 31;
+        double height = 180.1;
+        String city = "Seoul";
+
+        System.out.println("num");
+        System.out.println(num);
+    }
 }
